d1306bf2651a7db4371aa104430f140570a58300
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-13 14:10:40 +0200

    Delete post

--- routes/posts.js
+++ routes/posts.js
@@ -58,4 +58,16 @@ router.put("/:id", (req, res) => {
   res.status(201).json(posts);
 });
 
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res.status(400).json({ msg: `A post with id ${id} was not found` });
+  }
+
+  posts = posts.filter((post) => post.id !== id);
+  res.status(201).json(posts);
+});
+
 export default router;
