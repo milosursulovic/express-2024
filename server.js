b3085b9094232873bf77f7a3fb11ef92c06a872e
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-13 13:59:30 +0200

    Implement router

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import posts from "./routes/posts.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,32 +13,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-let posts = [
-  { id: 1, title: "Post One" },
-  { id: 2, title: "Post Two" },
-  { id: 3, title: "Post Three" },
-];
-
-app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
-
-  if (!isNaN(limit) && limit > 0) {
-    res.status(200).json(posts.slice(0, limit));
-  } else {
-    res.status(200).json(posts);
-  }
-});
-
-app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-
-  if (!post) {
-    res.status(404).json({ msg: `A post with the id of ${id} was not found` });
-  } else {
-    res.status(200).json(post);
-  }
-});
+app.use("/api/posts", posts);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
