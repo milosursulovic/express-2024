7faa989d0448428741242b7a33390b6e1352f51e
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-13 12:37:05 +0200

    Add some json

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ message: "Hello World" });
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  { id: 1, title: "Post One" },
+  { id: 2, title: "Post Two" },
+  { id: 3, title: "Post Three" },
+];
+
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
 });
 
 app.listen(PORT, () => {
