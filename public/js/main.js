e442293a1ee2e945b87e35a0ea7e17be8965543f
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-13 17:58:17 +0200

    Add form for creating post

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const output = document.querySelector("#output");
 const button = document.querySelector("#get-posts-btn");
+const form = document.querySelector("#add-post-form");
 
 async function showPosts() {
   try {
@@ -22,4 +23,34 @@ async function showPosts() {
   }
 }
 
+async function addPost(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  const title = formData.get("title");
+
+  try {
+    const res = await fetch("http://localhost:5000/api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title }),
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to add post");
+    }
+
+    const newPost = await res.json();
+
+    const postEl = document.createElement("div");
+    postEl.textContent = newPost.title;
+    output.appendChild(postEl);
+    showPosts();
+  } catch (error) {
+    console.log("Error adding post: ", error);
+  }
+}
+
 button.addEventListener("click", showPosts);
+form.addEventListener("submit", addPost);
