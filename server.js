8b3fa2be92106a6952f46cb92ce981edc8339c61
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-13 12:45:36 +0200

    Add limit query param

--- server.js
+++ server.js
@@ -19,7 +19,18 @@ let posts = [
 ];
 
 app.get("/api/posts", (req, res) => {
-  res.json(posts);
+  const limit = parseInt(req.query.limit);
+
+  if (!isNaN(limit) && limit > 0) {
+    res.json(posts.slice(0, limit));
+  } else {
+    res.json(posts);
+  }
+});
+
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json(posts.filter((post) => post.id === id));
 });
 
 app.listen(PORT, () => {
