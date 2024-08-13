5a182f82603910c5f949b935ebfa7224acb477fe
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-13 17:11:16 +0200

    Add custom error handler

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 import posts from "./routes/posts.js";
 import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/error.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -18,6 +19,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(logger);
 app.use("/api/posts", posts);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
